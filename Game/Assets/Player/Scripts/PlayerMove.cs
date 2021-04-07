0e395b6faf993292dbfd5151340b3286d3a7f81a
Matheus Guedes <mathheus.gr@gmail.com>
2021-04-07 11:12:29 -0300

    Movimentação
    animações importadas e programada a movimentação

---
+++ Game/Assets/Player/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Animator anim;
+    public float velRotacao;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", Input.GetAxis("Vertical"));
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * Time.deltaTime * velRotacao,0));
+    }
+}
